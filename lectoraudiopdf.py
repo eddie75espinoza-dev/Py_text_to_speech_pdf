7317c6303712b5b942bf2e61aeb6fad26ade70a4
eddie75dev <eddie75espinoza@gmail.com>
2021-04-09 23:35:27 -0300

    implementacion avanzada

---
+++ lectoraudiopdf.py
@@ -0,0 +1,62 @@
+import os
+import PyPDF2 # lector de pdf
+import pyttsx3 # speak
+
+CURR_DIR = os.path.dirname(os.path.realpath(__file__)) # tambien se puede usar = CURR_DIR = os.getcwd()
+speak = pyttsx3.init() # object creation
+
+def get_text(page_pdf):
+    if validate_page:
+        from_page = pdf_reader().getPage(page_pdf) #get details from pdf's page
+        text = from_page.extractText()    
+    return text
+
+def pdf_reader():
+    pdfReader = PyPDF2.PdfFileReader(path)    
+    return pdfReader
+
+
+def validate_page(page_init, page_end):
+    pages_pdf = pdf_reader().numPages
+    if page_init > pages_pdf:
+        print(f'Page not found, verify page initial')
+        return False
+    elif page_end > pages_pdf:
+        print(f'Page not found, end page = {pages_pdf}')
+        return False
+    return True
+
+def play_voice(text_languaje):  
+    ''' Changing Voices '''  
+    voices = speak.getProperty('voices')
+    
+    if text_languaje == 0:
+        return speak.setProperty('voice', voices[0].id) #changing index, changes voices. 0 for female in Spanish
+    if text_languaje == 1:
+        return speak.setProperty('voice', voices[1].id) #changing index, changes voices. 1 for female in English
+    if text_languaje == 2:
+        return speak.setProperty('voice', voices[2].id) #changing index, changes voices. 2 for male in English
+
+def playPdf(page_init, page_end, text_languaje):
+    page_init -=1
+    speak = pyttsx3.init()    
+    play_voice(text_languaje)    
+    if validate_page(page_init, page_end):
+        i = page_init
+        for i in range(i, page_end):              
+            speak.say(get_text(i))             
+            speak.runAndWait()
+            speak.stop()
+
+if __name__ == '__main__':
+    name_file = input('Enter the name of the .pdf file: ').strip()
+    path = open(CURR_DIR + '/' + name_file, 'rb')
+    text_languaje = int(input('''Indicates the file language:
+~ 0- Spanish / Female 
+~ 1- English / Female
+~ 2- English / Male: '''))    
+    page_init = input('Initial page: ').strip()
+    page_end = input('End page: ').strip()
+    playPdf(int(page_init), int(page_end), text_languaje)
+
+    
---
+++ readme.md
@@ -0,0 +1,20 @@
+✨ Aplicación desarrollada en Python 3 que permite 
+reconocer un texto de un archivo .pdf y lo convierte a
+audio usando la voz instalada en el sistema.
+
+Librerias usadas: 
+
+pyttsx3 (is a text-to-speech conversion library in Python)
+Offline Text To Speech (TTS) converter for Python3
+Más información en: https://github.com/nateshmbhat/pyttsx3
+
+PyPDF2 (A Pure-Python library built as a PDF toolkit)
+Esta librería permite manipular los archivos PDF.
+Más información en: http://mstamy2.github.io/PyPDF2/
+
+Uso:
+~ Permite seleccionar el archivo en la carpeta del proyecto.
+~ Permite seleccionar la voz nativa del sistema
+~ Puede seleccionar la pagina del archivo a escuchar
+
+Es posible crear archivos con formato .mp3 (no desarrollado) con estas librerías.
